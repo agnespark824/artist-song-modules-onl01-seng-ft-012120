aca7c5b103f266b8e8668d940c96239fcae028b4
Agnes Park <agnespark824@gmail.com>
2020-02-08 17:09:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
+  include Memorable::InstanceMethods, Paramble 
   
   attr_accessor :name
   attr_reader :songs
